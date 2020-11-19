78d0fd3681351243ed980963f59a5651699bda8f
root <mateusz.machnio.9@gmail.com>
2020-11-19 13:34:13 +0100

    trial configuration of SecurityConfig

--- src/main/java/pl/machnio/shoppingList/security/SecurityConfig.java
+++ src/main/java/pl/machnio/shoppingList/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pl.machnio.shoppingList.app;
+package pl.machnio.shoppingList.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,12 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity

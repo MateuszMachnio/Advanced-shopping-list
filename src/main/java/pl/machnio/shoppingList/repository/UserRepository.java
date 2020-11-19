85be6f598a7751162b79bad356f2dc64199c86ff
root <mateusz.machnio.9@gmail.com>
2020-11-19 13:33:21 +0100

    added UserRepository

--- src/main/java/pl/machnio/shoppingList/repository/UserRepository.java
+++ src/main/java/pl/machnio/shoppingList/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package pl.machnio.shoppingList.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.machnio.shoppingList.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }

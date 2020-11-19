cca0040fd111db75cb104f25151668f581955a09
root <mateusz.machnio.9@gmail.com>
2020-11-19 21:51:34 +0100

    complementing methods

--- src/main/java/pl/machnio/shoppingList/service/UserServiceImpl.java
+++ src/main/java/pl/machnio/shoppingList/service/UserServiceImpl.java
@@ -1,25 +1,46 @@
 package pl.machnio.shoppingList.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.machnio.shoppingList.entity.User;
 import pl.machnio.shoppingList.repository.UserRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
-
 
     @Override
     public User findById(long id) {
-        return null;
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("There is no such User"));
+    }
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        Optional<User> entity = userRepository.findById(user.getId());
+        if (entity.isPresent()) {
+            User updatedUser = entity.get();
+            if(!user.getPassword().equals(updatedUser.getPassword())){
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+            }
+            userRepository.save(user);
+        }
     }
 
     @Override
@@ -28,27 +49,13 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public void saveUser(User user) {
-
-    }
-
-    @Override
-    public void updateUser(User user) {
-
-    }
-
-    @Override
     public void deleteUserById(long id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
     public List<User> findAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
-    @Override
-    public boolean isUserInDatabase(Integer id, String email) {
-        return false;
-    }
 }

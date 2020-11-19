bb764991a675ce9180901c7ecf84ff35e3903317
root <mateusz.machnio.9@gmail.com>
2020-11-19 21:43:28 +0100

    rename of package

--- src/main/java/pl/machnio/shoppingList/controller/HomeController.java
+++ src/main/java/pl/machnio/shoppingList/controller/HomeController.java
@@ -1,4 +1,4 @@
-package pl.machnio.shoppingList.web;
+package pl.machnio.shoppingList.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

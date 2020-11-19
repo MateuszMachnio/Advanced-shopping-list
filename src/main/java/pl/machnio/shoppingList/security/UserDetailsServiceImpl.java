3876426b044db6fbcb5b37fcf50e6cd43bf4a59f
root <mateusz.machnio.9@gmail.com>
2020-11-19 15:45:37 +0100

    changed implementation of UserDetailsService

--- src/main/java/pl/machnio/shoppingList/security/UserDetailsServiceImpl.java
+++ src/main/java/pl/machnio/shoppingList/security/UserDetailsServiceImpl.java
@@ -1,24 +1,44 @@
 package pl.machnio.shoppingList.security;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import pl.machnio.shoppingList.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.machnio.shoppingList.entity.User;
+import pl.machnio.shoppingList.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private UserService userService;
 
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
     }
+
+
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        //todo later
-        return userRepository.findByEmail(s).get();
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+        if(user==null){
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                true, true, true, true, getGrantedAuthority(user));
     }
 
+
+    private List<GrantedAuthority> getGrantedAuthority(User user){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+user.getRole()));
+        return authorities;
+    }
 }

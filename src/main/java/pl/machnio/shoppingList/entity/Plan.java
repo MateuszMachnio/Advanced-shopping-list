15187b3a761c1cc9dc08614b004fce0f4383a807
root <mateusz.machnio.9@gmail.com>
2020-11-21 14:00:58 +0100

    supplementing of Plan entity

--- src/main/java/pl/machnio/shoppingList/entity/Plan.java
+++ src/main/java/pl/machnio/shoppingList/entity/Plan.java
@@ -1,7 +1,13 @@
 package pl.machnio.shoppingList.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -12,8 +18,91 @@ public class Plan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 50)
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "created_on")
+    private LocalDateTime created;
+
+    @Column(name = "updated_on")
+    private LocalDateTime updated;
+
     @ManyToMany
     @JoinTable(name = "plan_recipes", joinColumns = @JoinColumn(name = "plan_id"), inverseJoinColumns = @JoinColumn(name = "recipe_id"))
     private Set<Recipe> recipes = new HashSet<>();
 
+    @PrePersist
+    public void setCreated() {
+        this.created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void setUpdated() {
+        this.updated = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Plan plan = (Plan) o;
+        return id.equals(plan.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Plan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
 }

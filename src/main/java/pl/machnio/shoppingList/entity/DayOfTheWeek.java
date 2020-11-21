cff8a9dcfb18fe7a66528c9c823eeb58c581f6e4
root <mateusz.machnio.9@gmail.com>
2020-11-21 14:48:36 +0100

    supplementing of DayOfTheWeek entity

--- src/main/java/pl/machnio/shoppingList/entity/DayOfTheWeek.java
+++ src/main/java/pl/machnio/shoppingList/entity/DayOfTheWeek.java
@@ -1,6 +1,7 @@
 package pl.machnio.shoppingList.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "day_of_the_week")
@@ -10,5 +11,44 @@ public class DayOfTheWeek {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "day_name", nullable = false, unique = true, length = 10)
+    private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DayOfTheWeek that = (DayOfTheWeek) o;
+        return id.equals(that.id) &&
+                name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "DayOfTheWeek{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }

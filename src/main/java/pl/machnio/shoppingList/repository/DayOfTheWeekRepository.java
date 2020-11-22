496be0b7774df6e78b4e1c967a2974d81a0ed7e5
root <mateusz.machnio.9@gmail.com>
2020-11-22 09:13:01 +0100

    renamed

--- src/main/java/pl/machnio/shoppingList/repository/DayOfTheWeekRepository.java
+++ src/main/java/pl/machnio/shoppingList/repository/DayOfTheWeekRepository.java
@@ -2,7 +2,8 @@ package pl.machnio.shoppingList.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.machnio.shoppingList.entity.DayOfTheWeek;
 
 @Repository
-public interface DayOfWeekRepository extends JpaRepository<DayOfWeekRepository, Long> {
+public interface DayOfTheWeekRepository extends JpaRepository<DayOfTheWeek, Long> {
 }

dc90a976ea32dec6f43b8179eb6c3c9ac0aca26c
root <mateusz.machnio.9@gmail.com>
2020-11-23 19:32:46 +0100

    temporary access for all

--- src/main/java/pl/machnio/shoppingList/security/SecurityConfig.java
+++ src/main/java/pl/machnio/shoppingList/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/user/*", "/static/**").permitAll()
+                .antMatchers("/**", "/user/*", "/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()

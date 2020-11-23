ac119386362b4251d4d2d6b37a209b6632083698
root <mateusz.machnio.9@gmail.com>
2020-11-23 19:36:55 +0100

    displaying dashboard and adding plan view

--- src/main/java/pl/machnio/shoppingList/controller/LoggedUserController.java
+++ src/main/java/pl/machnio/shoppingList/controller/LoggedUserController.java
@@ -1,11 +1,24 @@
 package pl.machnio.shoppingList.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.machnio.shoppingList.entity.Plan;
 
 @Controller
 @RequestMapping("/logged_user")
 public class LoggedUserController {
 
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "logged-user/dashboard";
+    }
+
+    @GetMapping("/add-plan")
+    public String addPlan(Model model) {
+        model.addAttribute("plan", new Plan());
+        return "logged-user/addPlan";
+    }
 
 }
